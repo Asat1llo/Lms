c02b4ceb213ef35c17885ffd5fc21d87b4a2324b
Asat1llo <asatilloiminjonov57@gmail.com>
2023-12-21 23:40:35 +0500

    push chagnes

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const elAteg1 = document.querySelector('.lms__link_a__text1')
 const elAteg2 = document.querySelector('.lms__link_a__text2')
 const elAteg3 = document.querySelector('.lms__link_a__text3')
 const elAteg4 = document.querySelector('.lms__link_a__text4')
-const elAteg5= document.querySelector('.lms__link_a__text5')
+const elAteg5 = document.querySelector('.lms__link_a__text5')
 const elAteg6 = document.querySelector('.lms__link_a__text6')
 const elAteg7 = document.querySelector('.lms__link_a__text7')
-const elHeadr = document.querySelector('.lms__header__item1')
-const elNavbar = document.querySelector('lms__navbar__wrraper')
+
+const elHeader = document.querySelector('.header__section')
+const elNavbar = document.querySelector('.navbar__section')
+const elMain = document.querySelector('.main__section')
+
+const elBtn = document.querySelector('.navbar__wrraper__container__izoh__wrraper_btn__container__btn')
+const elRol = document.querySelector('.navbar__wrraper__container__izoh__wrraper_btn__container__btn__rol')
+
 
 
 elNavigatioon2.addEventListener('click', function () {
@@ -21,6 +27,12 @@ elNavigatioon2.addEventListener('click', function () {
     elAteg5.classList.toggle('active__text5')
     elAteg6.classList.toggle('active__text6')
     elAteg7.classList.toggle('active__text7')
-    elHeadr.classList.toggle('active__header')
+    elHeader.classList.toggle('active__header')
     elNavbar.classList.toggle('active__navbar')
+    elMain.classList.toggle('active__main')
 })
+
+elBtn.addEventListener('click', function () {
+    elRol.classList.toggle('active__rol')
+})
+
